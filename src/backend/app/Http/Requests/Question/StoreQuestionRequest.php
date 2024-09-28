0b8eace9344cbfb01fab27bf02f9336b48ced1ca
shtk0llq <shtk0llq@gmail.com>
2024-09-28 18:49:29 +0900

    create question feature

---
+++ src/backend/app/Http/Requests/Question/StoreQuestionRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\Question;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreQuestionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required'],
+            'content' => ['required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'タイトルが必須です',
+            'content.required' => '内容が必須です',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): HttpResponseException
+    {
+        $response = response()->json([
+            'error' => [
+                'code' => 400,
+                'message' => 'Invalid request parameter',
+                'errors' => $validator->errors(),
+            ],
+        ], 400);
+
+        throw new HttpResponseException($response);
+    }
+}
--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/questions', [QuestionController::class, 'store']);
