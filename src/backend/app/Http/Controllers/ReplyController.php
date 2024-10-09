ce30551d78eb4ae153f50ad12fa0f9335fe0c6de
shtk0llq <shtk0llq@gmail.com>
2024-10-09 18:15:34 +0900

    create reply update feature

--- src/backend/app/Http/Controllers/ReplyController.php
+++ src/backend/app/Http/Controllers/ReplyController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Reply\StoreReplyRequest;
+use App\Http\Requests\Reply\UpdateReplyRequest;
 use App\Http\Resources\Reply\ReplyResource;
 use App\Models\Comment;
+use App\Models\Reply;
 use Illuminate\Http\JsonResponse;
 
 class ReplyController extends Controller
@@ -16,5 +18,14 @@ class ReplyController extends Controller
         $reply = $comment->replies()->create($validated);
 
         return response()->json(new ReplyResource($reply), 201);
+    }
+
+    public function update(UpdateReplyRequest $request, Comment $comment, Reply $reply): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $reply->update($validated);
+
+        return response()->json(new ReplyResource($reply), 200);
     }
 }
