c381f200f468d2038745635622a908375d76eb2b
Shinagawa Takanori <119676984+shtk0llq@users.noreply.github.com>
2024-11-03 21:00:05 +0900

    reply controller update (#114)

--- src/backend/app/Http/Requests/Reply/DestroyReplyRequest.php
+++ src/backend/app/Http/Requests/Reply/DestroyReplyRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Reply;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class UpdateReplyRequest extends FormRequest
+class DestroyReplyRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,19 +25,7 @@ class UpdateReplyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'content' => ['required'],
-        ];
-    }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array<string, string>
-     */
-    public function messages(): array
-    {
-        return [
-            'content.required' => '返信内容は必須です',
+            //
         ];
     }
 }
