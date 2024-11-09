85fdc487edb3f15472694613789ff08ddd4548b6
shtk0llq <shtk0llq@gmail.com>
2024-11-09 18:36:17 +0900

    create question like feature

--- src/backend/app/Models/User.php
+++ src/backend/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,16 +13,33 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The likes that belong to the user.
+     */
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany(Question::class, 'likes', 'user_id', 'question_id')->withTimestamps();
+    }
+
+    /**
+     * Get the questions for the user.
+     */
     public function questions(): HasMany
     {
         return $this->hasMany(Question::class);
     }
 
+    /**
+     * Get the comments for the user.
+     */
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
 
+    /**
+     * Get the replies for the user.
+     */
     public function replies(): HasMany
     {
         return $this->hasMany(Reply::class);
