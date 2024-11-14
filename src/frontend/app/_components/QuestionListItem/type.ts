daed935c69f3ad5f60541554216ec6e2f5006c25
shtk0llq <shtk0llq@gmail.com>
2024-11-14 14:26:32 +0900

    question list item update

--- src/frontend/app/_components/QuestionListItem/type.ts
+++ src/frontend/app/_components/QuestionListItem/type.ts
@@ -3,6 +3,9 @@ export interface Question {
   title: string;
   content: string;
   is_resolved: boolean;
+  comments_count: number;
+  likes_count: number;
+  views_count: number;
   created_at: string;
   updated_at: string;
   categories: Category[];
