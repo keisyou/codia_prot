192ee14ad53714de531111df5e717f46e58d3858
shtk0llq <shtk0llq@gmail.com>
2024-11-26 01:07:51 +0900

    question type fix

--- src/frontend/api/questions/getQuestions/type.ts
+++ src/frontend/api/questions/getQuestions/type.ts
@@ -3,9 +3,12 @@ export interface Question {
   title: string;
   content: string;
   is_resolved: boolean;
+  categories: Category[];
+  comments_count: number;
+  likes_count: number;
+  views_count: number
   created_at: string;
   updated_at: string;
-  categories: Category[];
 }
 
 export interface Category {
